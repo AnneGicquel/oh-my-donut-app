c635339838d4062ea174185f10e41809b849e456
ASLAN-Erdal <aslan.erdal.dev@gmail.com>
2023-07-22 11:32:05 +0200

    header image and css added partially

--- src/pages/partials/header/Header.tsx
+++ src/pages/partials/header/Header.tsx
@@ -1,7 +1,11 @@
+import './Header.css';
+
 const Header = () => {
     return (
-        <header>
-            <img src="#" alt="header image background" />
+        <header className='header'>
+            <div className='header-image'></div>
+            {/* <img className='header-image' src="/assets/images/oh-my-donut-images/CATEGORY/category-douceurs.jpg" alt="header background" /> */}
+            <img className='header-basket' src="/assets/images/oh-my-donut-images/ICONS/basket.png" alt="header background" />
         </header>
     )
 }
