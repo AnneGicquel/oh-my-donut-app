f914b403ec87638c4f83a696040889f0a3c8bd6e
AnneGicquel <annelaedere@gmail.com>
2023-08-18 11:45:44 +0200

    Création dossier 'ReusableDropdown' et fichiers '.tsx' et '.module.css'

---
+++ src/components/common/reusableDropdown/reusableDropdown.tsx
@@ -0,0 +1,102 @@
+/** IMPORT **/
+import React, { useEffect, useRef, useState } from "react";
+
+import style from "components/ReusableDropdown/ReusableDropdown.module.css";
+
+
+/** PROPS **/
+interface DropdownItem {
+  id: number;
+  title: React.ReactNode;
+  subCategories?: DropdownItem[];
+}
+
+interface ReusableDropdownProps {
+  items: DropdownItem[];
+  onMenuItemClick: (itemId: number) => void;
+  onSubMenuItemClick: (itemId: number, subItemId: number) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // Ajout de la nouvelle prop !!!!!!!!
+}
+
+/** COMPONENT **/
+const ReusableDropdown = ({
+  items,
+  onMenuItemClick,
+  onSubMenuItemClick,
+}: ReusableDropdownProps) => {
+  const [openItems, setOpenItems] = useState<number[]>([]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const test = () => {
+    console.log("TESTTT");
+    // Logique pour gérer le clic sur les checkboxes
+  };
+
+  const toggleSubMenu = (itemId: number) => {
+    setOpenItems((prevOpenItems) =>
+      prevOpenItems.includes(itemId)
+        ? prevOpenItems.filter((id) => id !== itemId)
+        : [...prevOpenItems, itemId]
+    );
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      setOpenItems([]);
+    }
+  };
+
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className={style.dropdown} ref={dropdownRef}>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <div
+              className={style["menu-item"]}
+              onClick={() => {
+                toggleSubMenu(item.id);
+                onMenuItemClick(item.id);
+              }}
+            >
+              <span className={style["menu-item-text"]}>{item.title}</span>
+              <span className={style.chevron}>
+                {openItems.includes(item.id) ? "⌃" : "⌄"}
+              </span>
+            </div>
+            {item.subCategories && openItems.includes(item.id) && (
+              <ul className={style.subMenu}>
+                {item.subCategories.map((subItem) => (
+                  <li
+                    key={subItem.id}
+                    className={style["subMenu-child"]}
+                    onClick={() => onSubMenuItemClick(item.id, subItem.id)}
+                  >
+                    <div>
+                      {/* <ReusableCheckbox 
+                        label={subItem.title} 
+                        callback={test}
+                         /> */}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ReusableDropdown;
+
+
+
