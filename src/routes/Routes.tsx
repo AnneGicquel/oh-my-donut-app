f57016034bfb5e5a8db2cb7bac8a96e9dbc13478
AnneGicquel <annelaedere@gmail.com>
2023-08-18 11:57:11 +0200

    Création route ProductDetails

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import NotFound from "../pages/notFound/NotFound";
 import Home from "../pages/home/Home";
 import Products from "../pages/products/Products";
 import ProductProvider from "../contexts/ProductContext";
+import ProductDetails from "pages/productDetails/ProductDetails";
 
 const ROUTES: RouteObject[] = [
   {
@@ -20,6 +21,10 @@ const ROUTES: RouteObject[] = [
     path: "*",
     element: <NotFound />,
   },
+  {
+    path: "/ProductDetails",
+    element: <ProductDetails />
+},
 ];
 
 export { ROUTES };
