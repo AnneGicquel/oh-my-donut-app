b2c084f1600dc32b84ebad624c074c0a77225c3f
AnneGicquel <annelaedere@gmail.com>
2023-08-28 17:25:59 +0200

    Ajout de conditions pour display menu, message erreur, fonction onClick sur Button prop, et reprise de la page 404 (dont ajout dossier NOTFOUND et image test 404

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,12 +1,16 @@
 import Footer from "pages/partials/footer/Footer";
 import Header from "pages/partials/header/Header";
+import style from "./NotFound.module.css"
 
 const NotFound = () => {
     return (
         <>
-            <Header />
-            <img src="/assets" alt="" />
-            <Footer />
+            {/* <Header /> */}
+            <section className={style.anim} >
+                {/* <img src="assets/images/oh-my-donut-images/404_test.png" alt="" /> */}
+                {/* background img to anim with mediaQueries */}
+            </section>
+            {/* <Footer /> */}
         </>
 
     )
