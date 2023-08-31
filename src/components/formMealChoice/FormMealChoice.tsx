6a6764e8fa466548274676f381a59fd5d87d6c7e
JulieNonnon <julie.nonnon@gmail.com>
2023-08-31 09:47:48 +0200

    FEATURE: add style to FormMealChoice, responsive to be implemented

--- src/components/formMealChoice/FormMealChoice.tsx
+++ src/components/formMealChoice/FormMealChoice.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StayOrGoType } from 'interfaces/donuts.interface';
 import { useStayOrGoContext } from 'contexts/FormMealChoiceContext';
+import styles from './FormMealChoice.module.css';
 
 interface ChoiceOptionProps {
   type: StayOrGoType;
@@ -18,11 +19,22 @@ const ChoiceOption: React.FC<ChoiceOptionProps> = ({ type, imageSrc, title, desc
   };
 
   return (
+    
+
     <div className={`choice-option ${isSelected ? 'selected' : ''}`} onClick={handleSelect}>
-      <input type="radio" name="stayOrGo" checked={isSelected} readOnly />
-      <img src={imageSrc} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
+        
+      <div className={styles.formContainer}>
+
+        <input type="radio" name="stayOrGo" checked={isSelected} readOnly />
+        <img src={imageSrc} alt={title} />
+        
+        <div>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        
+      </div>
+
     </div>
   );
 };
--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -32,13 +32,13 @@ const Payment = () => {
       <div className="app">
         <ChoiceOption
           type="Emporter" //le radiobutton pour Emporter
-          imageSrc="oh-my-donut-app\public\assets\images\oh-my-donut-images\ICONS\velo.png"
+          imageSrc="assets/images/oh-my-donut-images/ICONS/velo.png"
           title="A Emporter"
           description="Après votre commande, veuillez vous rendre au comptoir 'A Emporter'. Bonne dégustation."
         />
         <ChoiceOption
           type="Sur place" //le radiobutton pour Sur Place
-          imageSrc="oh-my-donut-app\public\assets\images\oh-my-donut-images\ICONS\sac-de-courses.png"
+          imageSrc="assets/images/oh-my-donut-images/ICONS/sac-de-courses.png"
           title="Sur place"
           description="En échange de votre ticket, vous recevrez un bipper à l'accueil. Vous pouvez ensuite aller vous asseoir confortablement. Nous nous occupons du reste. "
         />
