7b55c0bd2aa5f836527bfcc57241d666574264da
Gitlab CI <ci@renault.com>
2023-08-31 23:00:51 +0200

    some bugs fixed

--- src/pages/productDetails/ProductDetails.tsx
+++ src/pages/productDetails/ProductDetails.tsx
@@ -1,18 +1,25 @@
 import ProductCustomizer from "components/productCustomizer/ProductCustomizer";
 import ProductDetailsComponent from "components/productDetailsComponent/ProductDetailsComponent";
+import { useCartContext } from "contexts/CartContext";
 import CategoryProvider from "contexts/CategoryContext";
 import MobileProvider from "contexts/MobileContext";
 import Navbar from "pages/partials/navbar/Navbar";
 import style from "pages/productDetails/ProductDetails.module.css"
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ProductDetails = () => {
+
+    const { getProductsFromCart } = useCartContext();
 
     const location = useLocation();
     useLayoutEffect(() => {
       window.scrollTo(0, 0);
     }, [location.pathname]);
+
+    useEffect(() => {
+        getProductsFromCart();
+    }, []);
   
 
     return (
